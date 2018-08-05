9d779910efd6ff7ddd107fd611b9c7b2d4a120ba
minimice <skyjuicesoftware@gmail.com>
2018-08-05 21:34:17 +0200

    Make port a variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const controllers = require('./controllers');
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 
 app.use('/', controllers);
 app.listen(port, function() {
