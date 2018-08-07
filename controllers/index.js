b805969325c16e41fe9f2a42057c9c426c0b8eca
Chooi-Guan Lim <chooi-guan.lim@scania.com>
2018-08-07 14:31:41 +0200

    Dockerize the app

--- controllers/index.js
+++ controllers/index.js
@@ -9,37 +9,38 @@ const allowedEmails = process.env.ALLOWEDEMAILSLOCAL || 'whiteList.txt';
 const customerEmailsRemoteUrl = process.env.ALLOWEDEMAILSREMOTE || ''; // Your fake list here
 const approvedFileToSend = process.env.SECUREDZIPFILE || './products/sample.zip';
 
-var customerEmails = [];
-downloader.download(customerEmailsRemoteUrl, allowedEmails)
-            .then((msg) => {
-              //console.log(msg);
-              customerEmails = fs.readFileSync(allowedEmails)
-                                 .toString()
-                                 .toLowerCase()
-                                 .split("\n")
-                                 .filter(e => e.indexOf("@") !== -1);
-              //only show emails
-              //var arrayLength = customerEmails.length;
-              //for (var i = 0; i < arrayLength; i++) {
-              //    console.log(customerEmails[i]);
-              //}
-            });
-
 router.get('/:email', function(req, res) {
   //var myemail = 'hello@world.com';
   const email = req.params['email'];
   console.log(email);
-  if (customerEmails.includes(email.trim())) {
-    //res.send('Approved to send file');
-    res.writeHead(200, {
-      'Content-Type': 'application/zip',
-      'Content-disposition': 'attachment; filename=' + email + '.zip'
+
+  // download email list
+  var customerEmails = [];
+  downloader.download(customerEmailsRemoteUrl, allowedEmails)
+    .then((msg) => {
+      //console.log(msg);
+      customerEmails = fs.readFileSync(allowedEmails)
+                        .toString()
+                        .toLowerCase()
+                        .split("\n")
+                        .filter(e => e.indexOf("@") !== -1);
+      //only show emails
+      //var arrayLength = customerEmails.length;
+      //for (var i = 0; i < arrayLength; i++) {
+      //    console.log(customerEmails[i]);
+      //}
+      if (customerEmails.includes(email.trim())) {
+        //res.send('Approved to send file');
+        res.writeHead(200, {
+          'Content-Type': 'application/zip',
+          'Content-disposition': 'attachment; filename=' + email + '.zip'
+        });
+        // name of the download to send if approved
+        fs.createReadStream(approvedFileToSend).pipe(res);
+      } else {
+        res.send('NOT Approved to send file');
+      }
     });
-    // name of the download to send if approved
-    fs.createReadStream(approvedFileToSend).pipe(res);
-  } else {
-    res.send('NOT Approved to send file');
-  }
 });
 
 router.get('/api/monitoring/ping', function(req, res) {
